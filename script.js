c2f309e6e8c7d633a275aefea46c00c9edf5195d
105372kl <105372kl@gmail.com>
2022-09-27 11:26:27 -0400

    1st

---
+++ script.js
@@ -0,0 +1,14 @@
+colors["b", "r", "y", "g", "c", "w"];
+main() {
+    createAnswer();
+}
+
+createAnswer() {
+    var answer = [];
+    let position = [];
+    for (let i = 0; i <= 4; i++) {
+        answer[position] = Math.floor(Math.random()*6);
+        position++;
+    }
+    alert(answer)
+}
